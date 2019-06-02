ce1345544374828f3d3c599b9a0d89082bef9211
Anatoly Popov <aensidhe@users.noreply.github.com>
2019-06-02 23:08:01 +0300

    Dockerize solution

--- Cwk.Ocelot.Demo.Gateway/Program.cs
+++ Cwk.Ocelot.Demo.Gateway/Program.cs
@@ -19,61 +19,56 @@ namespace Cwk.Ocelot.Demo.Gateway
     public class Program
     {
         private static IConfiguration _configuration;
-        public static void Main(string[] args)
+        public static void Main()
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder().Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            string ocelotFile = "ocelot.json";
-            string appSettingsFile = "appsettings.json";
-
-            return new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureAppConfiguration((hostingContext, config) =>
+        public static IWebHostBuilder CreateWebHostBuilder() => new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile("ocelot.json", false, true)
+                    .AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddEnvironmentVariables();
+                _configuration = config.Build();
+            })
+            .ConfigureServices(s =>
+            {
+                s.AddOcelot(_configuration);
+                _configuration.GetSection("GatewayOptions");
+                s.AddAuthentication(options =>
                 {
-                    config
-                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                        .AddJsonFile(appSettingsFile, true, true)
-                        .AddJsonFile(ocelotFile)
-                        .AddEnvironmentVariables();
-                    _configuration = config.Build();
-                })
-                .ConfigureServices(s =>
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer("TestKey", options =>
                 {
-                    s.AddOcelot(_configuration);
-                    IConfigurationSection section = _configuration.GetSection("GatewayOptions");
-
-                    s.AddAuthentication(options =>
+                    options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    }).AddJwtBearer("TestKey", options =>
-                    {
-                        options.TokenValidationParameters = new TokenValidationParameters
-                        {
-                            ValidateLifetime = true,
-                            ValidateIssuer = true,
-                            ValidIssuer = "DotNet Summit",
-                            ValidateAudience = false,
-                            ValidateIssuerSigningKey = true,
-                            IssuerSigningKey = new SymmetricSecurityKey(
-                                Encoding.ASCII.GetBytes(_configuration.GetSection("Authentication")["Secret"])),
-                            ValidateActor = false,
-                            RequireSignedTokens = true,
-                            RequireExpirationTime = false
-                        };
-                        options.RequireHttpsMetadata = false;
-                    });
-                    s.AddLogging( p => p.AddConsole());
-                })
-                .UseIISIntegration()
-                .Configure(app =>
-                {
-                    app.UseOcelot().Wait();
+                        ValidateLifetime = true,
+                        ValidateIssuer = true,
+                        ValidIssuer = "DotNet Summit",
+                        ValidateAudience = false,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.ASCII.GetBytes(_configuration.GetSection("Authentication")["Secret"])),
+                        ValidateActor = false,
+                        RequireSignedTokens = true,
+                        RequireExpirationTime = false
+                    };
+                    options.RequireHttpsMetadata = false;
                 });
-        }
+                s.AddLogging( p => p.AddConsole());
+            })
+            .UseIISIntegration()
+            .Configure(app =>
+            {
+                app.UseOcelot().Wait();
+            });
     }
 }
